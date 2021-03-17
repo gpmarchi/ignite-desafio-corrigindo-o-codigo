fe43d1a95614e3ae5ffcba885eed5159661a1dfc
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-03-17 13:09:39 -0300

    refactor: Get only fields allowed to be updated

--- src/index.js
+++ src/index.js
@@ -30,17 +30,15 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { url, title, techs } = request.body;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repository = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  Object.assign(repository, { url, title, techs });
 
   return response.json(repository);
 });
